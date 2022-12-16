a1c569127f70babf7e1d8c8fc61de444776d860d
Nassjedai <jedainassim@gmail.com>
2022-12-16 15:50:02 +0100

    Création ingrédient type par un formulaire sur symfo + personalisation contrainte, label + construction formulaire dans le controller new

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,7 +14,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+
+
+    /*
+     * This function display all ingredients
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient.index', methods:['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredient = $paginator->paginate(
@@ -24,4 +36,41 @@ class IngredientController extends AbstractController
             'ingredients' => $ingredient
         ]);
     }
+
+    #[Route ('/ingredient/nouveau', 'ingredient.new', methods: ['GET', 'POST'])]
+     public function new(
+         Request $request,
+         EntityManagerInterface $manager
+    ): Response
+
+     {
+         $ingredient = new Ingredient();
+         $form = $this->createForm(IngredientType::class, $ingredient);
+
+         $form->handleRequest($request);
+         if ($form ->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush($ingredient);
+
+             $this->addFlash(
+                 'success',
+                 'Votre ingrédient a été créé avec succès !'
+             );
+
+            // Problem with Route
+            //$this->redirectToRoute('ingredient.index');
+
+         } else {
+             $this->addFlash(
+                 'danger',
+                 "Votre ingrédient n'a pas été enregistré  !"
+             );
+         }
+
+        return $this->render('ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+     }
 }
